1037042d349fafab0ce0fbe2ee305125f5361a69
Gabriel Musskopf <gabrielgmusskopf@gmail.com>
2025-09-21 13:29:30 -0300

    feat: Interagir com a tabela de roteamento do kernel

--- router.py
+++ router.py
@@ -1,10 +1,12 @@
 import time
 import socket
+import struct
 import threading
 from dataclasses import dataclass, field
 from typing import List, Dict, Optional
 import argparse
 import ipaddress
+import subprocess
 
 # TODO:
 # Protocolo
@@ -13,16 +15,27 @@ import ipaddress
 # - [x] Receber a tabela de roteamento dos vizinhos
 # - [x] Atualizar a tabela de roteamento com as rotas recebidas
 # - [x] Expirar rota após TTL
+# - [x] Split horizon
 # Encaminhamento
 # - [ ] Encaminhar um pacote de dado com base na métrica
 # Kernel
-# - [ ] Inserir rota na tabela do kernel
-# - [ ] Remover rota da tabela do kernel
+# - [x] Inserir rota na tabela do kernel
+# - [x] Remover rota da tabela do kernel
 # Probes para latência e perda de pacote
 # - [ ] Enviar probe para latência
 # - [ ] Enviar probes para perda de pacote
 # - [ ] Responder probes
 # - [ ] Recalcular métrica da rota
+
+try:
+    # Tenta acessar o atributo para ver se ele existe. Atributo só foi adicionado na versão 3.12,
+    # então para versões antigas é preciso atribuir manualmente.
+    # https://docs.python.org/3/library/socket.html
+    socket.IP_PKTINFO
+except AttributeError:
+    # Se não existir, definir com o valor padrão do Linux
+    # https://github.com/torvalds/linux/blob/f975f08c2e899ae2484407d7bba6bb7f8b6d9d40/include/uapi/linux/in.h#L108
+    socket.IP_PKTINFO = 8
 
 
 @dataclass
@@ -58,6 +71,11 @@ class RoutingTable:
             if destination not in self._routes or new_route.metric < self._routes[destination].metric:
                 print(f"[RoutingTable] Updating route to {destination} via {new_route.next_hop}")
                 self._routes[destination] = new_route
+                self.add_kernel_route(
+                    destination=str(new_route.destination_network),
+                    gateway=str(new_route.next_hop)
+                )
+
             # Se a rota veio do mesmo vizinho, apenas renova o timestamp
             elif self._routes[destination].next_hop == new_route.next_hop:
                 self._routes[destination].timestamp = new_route.timestamp
@@ -75,12 +93,40 @@ class RoutingTable:
     def remove_expired_routes(self, timeout: int):
         """Varre a tabela e remove rotas que não são atualizadas há algum tempo."""
         with self._lock:
-            expired_destinations = [
-                dest for dest, route in self._routes.items() if route.is_expired(timeout)
+            expired_routes = [
+                route for route in self._routes.values() if route.is_expired(timeout)
             ]
-            for dest in expired_destinations:
+            for expired_route in expired_routes:
+                dest = expired_route.destination_network
                 print(f"[RoutingTable] Route to {dest} expired. Removing.")
                 del self._routes[dest]
+
+                self.del_kernel_route(
+                    destination=str(expired_route.destination_network),
+                    gateway=str(expired_route.next_hop)
+                )
+
+    def add_kernel_route(self, destination: str, gateway: str):
+        """Adiciona uma rota na tabela do kernel usando o comando 'ip'."""
+        if gateway == "0.0.0.0":
+            return
+
+        print(f"[Kernel] Adding route {destination} via {gateway} to kernel routing table")
+        command = ["ip", "route", "add", destination, "via", gateway]
+        print(f"[Kernel] { ' '.join(command) }")
+
+        subprocess.run(command, check=False)
+
+    def del_kernel_route(self, destination: str, gateway: str):
+        """Remove uma rota da tabela do kernel usando o comando 'ip'."""
+        if gateway == "0.0.0.0":
+            return
+
+        print(f"[Kernel] Removing route {destination} via {gateway} from kernel routing table")
+        command = ["ip", "route", "del", destination, "via", gateway]
+        print(f"[Kernel] { ' '.join(command) }")
+
+        subprocess.run(command, check=False)
 
     def get_printable_string(self) -> str:
         with self._lock:
@@ -105,6 +151,7 @@ class NetworkInterface:
     ip_interface: ipaddress.IPv4Interface
     mac_address: str
     state: str  # "UP" or "DOWN"
+    # TODO: atualizar estado da interface com base no kernel
 
 
 class ProtocolHandler:
@@ -122,15 +169,12 @@ class ProtocolHandler:
         # processos, quando esta no estádo de TIME_WAIT
         self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
 
+        # Pede ao kernel informações sobre a interface recebida. Usado para o split horizon
+        self.sock.setsockopt(socket.IPPROTO_IP, socket.IP_PKTINFO, 1)
+
         self.sock.bind(('', self.protocol_port))
 
-    def create_update_packet(self) -> bytes:
-        """Cria um pacote com as rotas da tabela para anunciar aos vizinhos."""
-        routes = self.routing_table.get_all_routes()
-        payload = ";".join([f"{r.destination_network},{r.metric}" for r in routes])
-        return payload.encode('utf-8')
-
-    def parse_incoming_packet(self, data: bytes, source_ip: str):
+    def parse_incoming_packet(self, data: bytes, source_ip: str, incoming_interface_name: str):
         """Interpreta um pacote recebido e atualiza a tabela de roteamento."""
         payload = data.decode('utf-8')
         received_routes = payload.split(';')
@@ -138,24 +182,33 @@ class ProtocolHandler:
         for route_str in received_routes:
             try:
                 dest_str, metric_str = route_str.split(',')
+                print(f"[ProtocolHandler] {source_ip} send route {dest_str} with metric {metric_str}")
                 new_entry = RouteEntry(
                     destination_network=ipaddress.IPv4Network(dest_str),
                     next_hop=ipaddress.IPv4Address(source_ip),
-                    metric=int(metric_str) + 1,  # Métrica do vizinho + 1 (custo do salto)
-                    outgoing_interface="TODO: determine interface"  # Determinar por qual interface o pacote chegou
+                    metric=int(metric_str) + 1,
+                    outgoing_interface=incoming_interface_name
                 )
                 self.routing_table.add_or_update_route(new_entry)
             except (ValueError, IndexError):
                 print(f"[ProtocolHandler] Malformed route data received from {source_ip}: {route_str}")
 
     def gossip_table(self, interfaces: List[NetworkInterface]):
-        """Envia pacotes de descoberta ou métricas para vizinhos."""
-        # A implementação depende da sua estratégia de métricas (latência, etc.)
-        # Exemplo: Enviar um pacote para o endereço de broadcast da rede local.
-        update_packet = self.create_update_packet()
+        """Envia a tabela de rotas para vizinhos."""
+        all_routes = self.routing_table.get_all_routes()
 
         for iface in interfaces:
             if iface.state == "UP":
+                routes_to_send = [
+                    route for route in all_routes if route.outgoing_interface != iface.name
+                ]
+
+                if not routes_to_send:
+                    continue
+
+                payload = ";".join([f"{r.destination_network},{r.metric}" for r in routes_to_send])
+                update_packet = payload.encode('utf-8')
+
                 broadcast_address = str(iface.ip_interface.network.broadcast_address)
                 print(f"[ProtocolHandler] Sending routing update via {iface.name} to {broadcast_address}")
                 self.sock.sendto(update_packet, (broadcast_address, self.protocol_port))
@@ -163,12 +216,25 @@ class ProtocolHandler:
     def listen_for_packets(self):
         """Loop principal para escutar pacotes de outros roteadores."""
         print("[ProtocolHandler] Listening for routing packets...")
+        if_map = {index: name for index, name in socket.if_nameindex()}
+
         while True:
             try:
-                data, addr = self.sock.recvfrom(1024)
+                data, ancdata, flags, addr = self.sock.recvmsg(1024, 1024)
                 source_ip = addr[0]
-                print(f"[ProtocolHandler] Received packet from {source_ip}")
-                self.parse_incoming_packet(data, source_ip)
+                if ipaddress.IPv4Address(source_ip) == self.router_id:
+                    continue
+
+                incoming_interface_name = "unknown"
+                for cmsg_level, cmsg_type, cmsg_data in ancdata:
+                    if cmsg_level == socket.IPPROTO_IP and cmsg_type == socket.IP_PKTINFO:
+                        # Ler um inteiro sem sinal ("I") do buffer
+                        if_index = struct.unpack_from("I", cmsg_data)[0]
+                        incoming_interface_name = if_map.get(if_index, "unknown")
+                        break
+
+                print(f"[ProtocolHandler] Received packet from {source_ip} at {incoming_interface_name} interface")
+                self.parse_incoming_packet(data, source_ip, incoming_interface_name)
             except OSError as e:
                 print(f"[ProtocolHandler] Socket error: {e}")
                 break
@@ -188,9 +254,7 @@ class Router:
         self.route_timeout = 30
 
     def discover_interfaces(self, interface_definitions: List[str]):
-        """
-        Processa as definições de interface passadas via argumento de linha de comando.
-        """
+        """Processa as definições de interface passadas via argumento de linha de comando."""
         print("[Router] Configuring interfaces from command line arguments...")
 
         if not interface_definitions:
